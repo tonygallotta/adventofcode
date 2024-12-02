f0f71c4db13c19cfb2ba133ef80c7d8b2ffc71ac
Tony Gallotta <anthony.gallotta@gmail.com>
2024-12-02 09:32:44 -0600

    Some cleanup

---
+++ 2024/common.py
@@ -0,0 +1,25 @@
+def read_input_as_int_arrays(day, sample = False):
+    results = []
+    with open(get_file_name(day, sample)) as file:
+        for line in file:
+            values = line.strip().split()
+            results.append([int(v) for v in values])
+    return results
+
+def read_input_as_string_arrays(day, sample):
+    results = []
+    with open(get_file_name(day, sample)) as file:
+        for line in file:
+            values = line.strip().split()
+            results.append(values)
+    return results
+
+def read_input_as_strings(day, sample):
+    results = []
+    with open(get_file_name(day, sample)) as file:
+        for line in file:
+            results.append(line.strip())
+    return results
+
+def get_file_name(day, sample):
+    return "input/day{}{}.txt".format(day, "_sample" if sample else "")
--- 2024/day2.py
+++ 2024/day2.py
@@ -1,58 +1,45 @@
-from collections import Counter
-
-reports = []
-
-with open("input/day2.txt") as file:
-    for line in file:
-        values = line.strip().split()
-        reports.append([int(v) for v in values])
-
+from common import read_input_as_int_arrays
 
 def check_is_safe(report, check_increasing):
-    is_safe = True
     for i in range(1, len(report)):
         prev_level = report[i - 1]
         current_level = report[i]
         diff = abs(current_level - prev_level)
         if check_increasing and current_level < prev_level:
-            is_safe = False
+            return False
         elif (not check_increasing) and current_level > prev_level:
-            is_safe = False
-        elif diff > 3 or diff == 0:
-            # print("Diff is %s between %s and %s, not safe" % (diff, current_level, prev_level))
-            is_safe = False
-    return is_safe
+            return False
+        elif diff == 0 or diff > 3:
+            return False
+    return True
 
-print(reports)
-answer = 0
-for r in range(len(reports)):
-    report = reports[r]
-    check_increasing = report[0] < report[1]
-    if check_is_safe(report, check_increasing):
-        answer += 1
-        # print("SAFE")
-    # else:
-    #     # print("UNSAFE")
-
-print("Part 1: %s" % answer)
-
-answer = 0
-for r in range(len(reports)):
-    report = reports[r]
-    is_safe = check_is_safe(report, True) or check_is_safe(report, False)
-    if is_safe:
-        print("report %s is safe AS IS" % r)
-        answer += 1
-        continue
-    for i in range(len(report)):
-        report_with_removal = report.copy()
-        del report_with_removal[i]
-        is_safe = check_is_safe(report_with_removal, True) or check_is_safe(report_with_removal, False)
-        if is_safe:
-            print("report %s is safe by deleting %s" % (r, i))
+if __name__ == "__main__":
+    reports = read_input_as_int_arrays(2)
+    answer = 0
+    for r in range(len(reports)):
+        report = reports[r]
+        if check_is_safe(report, report[0] < report[1]):
             answer += 1
-            break
-    else:
-        continue
 
-print("Part 2: %s" % answer)
+    print("Part 1: %s" % answer)
+
+    answer = 0
+    for r in range(len(reports)):
+        report = reports[r]
+        is_safe = check_is_safe(report, True) or check_is_safe(report, False)
+        if is_safe:
+            # print("report %s is safe AS IS" % r)
+            answer += 1
+            continue
+        for i in range(len(report)):
+            report_with_removal = report.copy()
+            del report_with_removal[i]
+            is_safe = check_is_safe(report_with_removal, True) or check_is_safe(report_with_removal, False)
+            if is_safe:
+                # print("report %s is safe by deleting %s" % (r, i))
+                answer += 1
+                break
+        else:
+            continue
+
+    print("Part 2: %s" % answer)
