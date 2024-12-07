0ab5b241da85584db17bbc43eb7c88951a181f5c
Tony Gallotta <anthony.gallotta@gmail.com>
2024-12-07 14:52:39 -0600

    Day 7 cleanup and part 2

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,4 +1,3 @@
-import itertools
 from dataclasses import dataclass
 
 from common import get_file_name
@@ -8,6 +7,52 @@ from common import get_file_name
 class Equation:
     test_value: int
     numbers: list[int]
+
+    def is_full_solution(self, operators: list[str]) -> bool:
+        if len(operators) != len(self.numbers) - 1:
+            return False
+        result = self.numbers[0]
+        for i in range(1, len(self.numbers)):
+            operator = operators[i - 1]
+            if operator == '+':
+                result += self.numbers[i]
+            elif operator == '*':
+                result *= self.numbers[i]
+            else:
+                result = int('{}{}'.format(result, self.numbers[i]))
+            if result > self.test_value:
+                return False
+        # print('Checking {} on {} -> {}'.format(operators, self, result))
+        return result == self.test_value
+
+    def is_viable_partial_solution(self, operators: list[str]) -> bool:
+        if len(operators) >= len(self.numbers):
+            return False
+        elif len(operators) == 0:
+            return True
+        result = self.numbers[0]
+        for i in range(1, len(operators)):
+            operator = operators[i - 1]
+            if operator == '+':
+                result += self.numbers[i]
+            elif operator == '*':
+                result *= self.numbers[i]
+            else:
+                result = int('{}{}'.format(result, self.numbers[i]))
+            if result > self.test_value:
+                return False
+        return len(operators) <= len(self.numbers) - 1
+
+
+def backtrack_dfs_solve(e: Equation, partial_solution: list[str], possible_operators: list[str]) -> bool:
+    if e.is_full_solution(partial_solution):
+        return True
+    elif not e.is_viable_partial_solution(partial_solution):
+        return False
+    else:
+        for operator in possible_operators:
+            if backtrack_dfs_solve(e, partial_solution + [operator], possible_operators):
+                return True
 
 
 if __name__ == "__main__":
@@ -19,27 +64,17 @@ if __name__ == "__main__":
             numbers = line.strip().split()[1:]
             equations.append(Equation(int(test_value), [int(n) for n in numbers]))
 
-    valid_test_values = set()
-    counter = 0
     for equation in equations:
-        # No way this will scale for pt 2
-        num_operators = len(equation.numbers) - 1
-        possible_operator_combos = list(itertools.product(*[['+', '*'] for i in range(num_operators)]))
-        solved = False
-        for ops in possible_operator_combos:
-            result = equation.numbers[0]
-            for i in range(1, len(equation.numbers)):
-                counter += 1
-                if ops[i - 1] == '+':
-                    result += equation.numbers[i]
-                else:
-                    result *= equation.numbers[i]
-            if result == equation.test_value:
-                # print('{} is solved with {}'.format(equation, ops))
-                valid_test_values.add(result)
-                solved = True
-                break
-            if solved:
-                break
+        if backtrack_dfs_solve(equation, [], ['+', '*']):
+            answer += equation.test_value
 
-    print("Part 1: {}".format(sum(valid_test_values)))
+    print("Part 1: {}".format(answer))
+
+    answer = 0
+
+    for idx, equation in enumerate(equations):
+        print('checking equation {}'.format(idx))
+        if backtrack_dfs_solve(equation, [], ['+', '*', '||']):
+            answer += equation.test_value
+
+    print("Part 2: {}".format(answer))
